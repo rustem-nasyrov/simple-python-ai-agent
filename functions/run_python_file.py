2e08bfd1476df4197c4636815bb9ce1058c44503
Rustem Nasyrov <nasyroff.rus@gmail.com>
2026-04-04 20:50:21 +0400

    feat: add function schema for run_python_file
    - Define structured parameters and descriptions
    - Enable metadata integration for the function call system

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -1,5 +1,7 @@
 import os
 import subprocess
+from google.genai import types
+
 
 def run_python_file(working_directory, file_path, args=None):
     try:
@@ -34,3 +36,32 @@ def run_python_file(working_directory, file_path, args=None):
         return '\n'.join(output)
     except Exception as e:
         return f"Error: executing Python file: {e}"
+
+
+schema_run_python_file = types.FunctionDeclaration(
+    name='run_python_file',
+    description='Executes a Python script within the specified working directory using a subprocess and returns the combined output (STDOUT/STDERR).',
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        required=[
+            'working_directory',
+            'file_path',
+            'args',
+        ],
+        properties={
+            'working_directory': types.Schema(
+                type=types.Type.STRING,
+                description='Directory path to run python file from',
+            ),
+            'file_path': types.Schema(
+                type=types.Type.STRING,
+                description='Path to the python file',
+            ),
+            'args': types.Schema(
+                type=types.Type.ARRAY,
+                items=types.Schema(type=types.Type.STRING),
+                description='An optional list of command-line arguments to pass to the script.',
+            ),
+        },
+    ),
+)
